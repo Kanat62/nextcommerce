4b6d9c8f273697fa7ed75ab7a7465a902b8e180a
Kanat62 <102471510+Kanat62@users.noreply.github.com>
2025-01-25 13:34:36 -0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
-export const PHONE_NUMBER: string = '0704030320'
-export const STORE_ADDRESS: string = 'ТЦ ЦУМ 0-ЭТАЖ G-24'
+export const PHONE_NUMBER: string = '0702999999'
+export const STORE_ADDRESS: string = 'ТЦ ЦУМ 4-этаж T-9'
 export const STORE_HOURS: string = 'c 10:00 до 21:00 без выходных'
 
-export const LINK_PHONE: string = 'tel:+996704030320'
-export const LINK_WHATSAPP: string = 'https://wa.me/996704030320'
-export const LINK_INSTAGRAM: string = 'https://www.instagram.com/sheku_tsum/'
+export const LINK_PHONE: string = 'tel:+996702999999'
+export const LINK_WHATSAPP: string = 'https://wa.me/996702999999'
+export const LINK_INSTAGRAM: string = 'https://www.instagram.com/test/'
